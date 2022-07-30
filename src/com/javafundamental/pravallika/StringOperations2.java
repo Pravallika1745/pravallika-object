b8b5cbc371f015cc0d1ed226d42f51cf6bb78f4a
Pravallika Kolluru <pravallika1745@gmail.com>
2022-07-29 23:59:47 -0500

    Adding additional string operationsn

--- src/com/javafundamental/pravallika/StringOperations2.java
+++ src/com/javafundamental/pravallika/StringOperations2.java
@@ -13,8 +13,11 @@ public class StringOperations2 {
 		System.out.println(date.substring(3,5));
 		System.out.println(date.substring(6,10));
 		
-		String wizard = "Pravallika Kolluru";
+		String wizard = "Harry Potter";
 		System.out.println(wizard.substring(0,wizard.indexOf(' ')));
+		System.out.println(wizard.substring(wizard.indexOf(' ')+1));
+		System.out.println(wizard.substring(wizard.indexOf(' ')+1).concat(", ").concat(wizard.substring(0,wizard.indexOf(' '))));
+		System.out.println(wizard.substring(0,1)+(".")+wizard.substring(wizard.indexOf(' ')+1,wizard.indexOf(" ")+2));
 	}
 
 }
